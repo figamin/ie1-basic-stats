969a7d2cb7a1f399d9d96b66c7f61c261b3690ba
Jiaqi Ye <jye387@gmail.com>
2024-02-20 16:44:31 -0500

    made min max calculator file

---
+++ MinMaxCalculation.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class MinMax {
+
+	public MinMax() {
+	}
+
+	public double getMax(ArrayList<Double> numbers) {
+		return Collections.max(numbers);
+	}
+
+    public double getMin(ArrayList<Double> numbers) {
+		return Collections.min(numbers);
+	}
+
+}
